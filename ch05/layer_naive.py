14f0aa5bf2e28844529b1842bc0598d05181ee57
Youngmi Park <yommi1121@naver.com>
2021-01-08 23:50:01 +0900

    덧셈 계층

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -4,7 +4,7 @@
 #곱셉 노드
 class MulLayer:
     def __init__(self):
-        #필요한 입력 없이 초기화
+        #필요한 입력없이 초기화
         self.x = None
         self.y = None
 
@@ -25,3 +25,26 @@ class MulLayer:
         return dx, dy
 
 #덧셈 노드
+class AddLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    #순전파
+    def forward(self,x, y):
+        self.x = x
+        self.y = y
+        out = x + y
+
+        return out
+
+    #역전파
+    def backward(self, dout):
+        #상류에서 내려온 미분을 그대로 흘려보낸다.
+        dx = dout * 1
+        dy = dout * 1
+
+        return dx, dy
+
+
+
