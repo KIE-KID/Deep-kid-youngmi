17a805e51bf72bb781d7f31c50cf647dac77e764
Youngmi Park <yommi1121@naver.com>
2021-01-08 23:31:09 +0900

    단순한 계층 구현하기

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -4,9 +4,11 @@
 #곱셉 노드
 class MulLayer:
     def __init__(self):
+        #필요한 입력 없이 초기화
         self.x = None
         self.y = None
 
+    #순전파
     def forward(self,x,y):
         self.x = x
         self.y = y
@@ -14,10 +16,12 @@ class MulLayer:
 
         return out
 
+    # 역전파
     def backward(self, dout):
-        #x y를 바꿔서 곱해준다.
+        #미분에 x y를 바꿔서 곱해준다.
         dx = dout * self.y
         dy = dout * self.x
 
         return dx, dy
+
 #덧셈 노드
