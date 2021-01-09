25e49be91306caa02face8eee2df21f8a2b58572
Youngmi Park <yommi1121@naver.com>
2021-01-09 16:38:17 +0900

    sigmoid layer

--- common/layers.py
+++ common/layers.py
@@ -1,3 +1,4 @@
+import numpy as np
 # ReLU 함수
 class Relu:
     def __init__(self):
@@ -13,5 +14,21 @@ class Relu:
     def backward(self, dout):
         dout[self.mask] = 0
         dx = dout
+
+        return dx
+
+#sigmoid
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1/(1+np.exp(-x))
+        self.out = out # y, 순전파의 출력을 보관해뒀다가 역전파 계산 때 사용
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.out * (1-self.out)
 
         return dx
