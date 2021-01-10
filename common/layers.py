84feff838565f8e0c7b9652e62cd5722dbf5a602
Youngmi Park <yommi1121@naver.com>
2021-01-10 23:22:03 +0900

    affine layer

--- common/layers.py
+++ common/layers.py
@@ -1,5 +1,5 @@
 import numpy as np
-# ReLU 함수
+# 4장 신경망 학습 ReLU
 class Relu:
     def __init__(self):
         self.mask = None
@@ -17,7 +17,7 @@ class Relu:
 
         return dx
 
-#sigmoid
+# 4장 신경망 학습 sigmoid
 class Sigmoid:
     def __init__(self):
         self.out = None
@@ -31,4 +31,36 @@ class Sigmoid:
     def backward(self, dout):
         dx = dout * self.out * (1-self.out)
 
+        return dx
+
+
+# 5장 오차역전파법 어파인 계층
+# 텐서 대응을 위한 함수로 교재에 있는 예제와 차이가 있음.
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+
+        self.x = None
+        self.original_x_shape = None
+        # 가중치와 편향 매개변수의 미분
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        # 텐서 대응
+        self.original_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
+
+        out = np.dot(self.x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0) # 열방햡으로 합계
+
+        dx = dx.reshape(*self.original_x_shape)  # 입력 데이터 모양 변경(텐서 대응)
         return dx
