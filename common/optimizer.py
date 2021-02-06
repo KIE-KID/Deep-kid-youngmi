dc9f833e4f1012f074fd427392a2f5c5f564683a
Youngmi Park <yommi1121@naver.com>
2021-02-06 20:10:19 +0900

    momentum

--- common/optimizer.py
+++ common/optimizer.py
@@ -8,3 +8,20 @@ class SGD:
     def update(self, params, grads):
         for key in params.keys():
             params[key] -= self.lr * grads[key]
+
+# 모멘텀
+class Momentum:
+    def __init__(self,lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None # 초기에 v는 아무것도 담지 않는다.
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val) # 매개변수와 동일한 구조의 데이터를 딕셔너리 변수로 저장
+
+        for key in params.keys():
+            self.v[key] = self.momentum * self.v[key] - self.lr * grads[key]
+            params[key] += self.v[key]
