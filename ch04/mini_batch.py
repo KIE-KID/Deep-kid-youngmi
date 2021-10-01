cfb5757ab0b2c35d34ec62be67f8b189e5c595e8
Youngmi-Park <yommi1121@naver.com>
2021-10-02 04:22:22 +0900

    Add mini batch

---
+++ ch04/mini_batch.py
@@ -0,0 +1,17 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+print(x_train.shape) # (60000, 784)
+print(t_train.shape) # (60000, 10)
+
+train_size = x_train.shape[0] # 60000
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size) # 60000개 중에서 10개를 랜덤으로 가져오는데, 인덱스를 가져옴.
+print(batch_mask) # [28616 40437 20310 35362 39939 57401  1441  7263 55281 33077], 매번 바뀜
+
+x_batch = x_train[batch_mask] # 해당 인덱스를 가지고 있는걸 반환
+t_batch = t_train[batch_mask]
